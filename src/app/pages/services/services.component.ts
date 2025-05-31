6330dd3b9dcecfe5adff69c7e8fe2a19ce80ec76
Ahmed Essam <40560225+iRashwan@users.noreply.github.com>
2025-05-31 15:59:17 +0300

    make some chagnes

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -17,116 +17,33 @@ export class ServicesComponent {
     {
       id: 0,
       name: 'services.service_1',
-      image: 'assets/images/icon-service-1.svg',
-      showDetails: false,
-      details: [
-       {id:0,
-          name:"services.s1_1"
-        },
-         {id:1,
-          name:"services.s1_2"
-        },
-        {
-          id:2,
-          name:'services.s1_3'
-        },
-         {id:3,
-          name:"services.s1_4"
-        },
-         {id:4,
-          name:"services.s1_5"
-        },
-      ],
+      image: 'assets/images/s-1.svg',
     },
     {
       id: 1,
       name: 'services.service_2',
-      image: 'assets/images/icon-sv-2.svg',
-      showDetails: false,
-      details: [
-        {id:0,
-          name:"services.s2_1"
-        },
-         {id:1,
-          name:"services.s2_2"
-        },
-        {
-          id:2,
-          name:'services.s2_3'
-        },
-         {id:3,
-          name:"services.s2_4"
-        },
-         {id:4,
-          name:"services.s2_5"
-        },
-        {
-          id:5,
-          name:'services.s2_6'
-        }
-      ],
+      image: 'assets/images/s-2.svg',
     },
     {
       id: 2,
       name: 'services.service_3',
-      image: 'assets/images/icon-sv-3.svg',
-      showDetails: false,
-      details: [
-       {id:0,
-          name:"services.s3_1"
-        },
-         {id:1,
-          name:"services.s3_2"
-        },
-        {
-          id:2,
-          name:'services.s3_3'
-        },
-         {id:3,
-          name:"services.s3_4"
-        },
-         {id:4,
-          name:"services.s3_5"
-        },
-        {
-          id:5,
-          name:'services.s3_6'
-        }
-      ],
+      image: 'assets/images/s-3.svg',
     },
     {
       id: 3,
       name: 'services.service_4',
-      image: 'assets/images/icon-sv-4.svg',
-      showDetails: false,
-      details: [
-        {id:1,
-          name:"services.s4_1"
-        },
-         {id:2,
-          name:"services.s4_2"
-        },
-        {
-          id:3,
-          name:'services.s4_3'
-        },
-         {id:4,
-          name:"services.s4_4"
-        },
-         {id:5,
-          name:"services.s4_5"
-        }
-      ],
+      image: 'assets/images/s-4.svg',
     },
+    {
+      id: 0,
+      name: 'services.service_4',
+      image: 'assets/images/s-5.svg',
+    },
+    {
+      id: 1,
+      name: 'services.service_6',
+      image: 'assets/images/s-6.svg',
+    }
   ];
-  showServiceDetails(id: number) {
-    this.services.map((res: any) => {
-      if (res.id == id) res.showDetails = true;
-    });
-  }
-  hideServiceDetails(id: number) {
-    this.services.map((res: any) => {
-      if (res.id == id) res.showDetails = false;
-    });
-  }
+
 }
