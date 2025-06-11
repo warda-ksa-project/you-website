aa3bdb26f8ab50e4046736566ad44cc523d7cff6
Ahmed Aladdin <ahmedaladdin@gmail.com>
2025-06-12 00:45:23 +0300

    add carousel

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,18 +1,20 @@
-import { Component, Inject, inject } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, inject, ViewChild } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { TranslateServiceService } from '../../services/translate-service.service';
-import { DOCUMENT, NgIf } from '@angular/common';
+import { DOCUMENT, NgIf, NgStyle } from '@angular/common';
 import { LocalStorageServiceService } from '../../services/local-storage-service.service';
 import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterModule,TranslatePipe,NgIf],
+  imports: [RouterModule,TranslatePipe,NgIf, NgStyle],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
-export class NavBarComponent {
+export class NavBarComponent implements AfterViewInit{
+    @ViewChild('carousel') carousel!: ElementRef;
+navBackGround='assets/images/slider1.svg'
   translateService = inject(TranslateServiceService);
   LocalStorageService =inject(LocalStorageServiceService)
   selectedLang: string = this.LocalStorageService.getItem('lang') ?? 'en';
@@ -33,5 +35,24 @@ export class NavBarComponent {
     this.document.body.dir = this.selectedLang === 'ar' ? 'rtl' : 'ltr';
     this.document.documentElement.setAttribute('lang', this.selectedLang);
     this.document.documentElement.setAttribute('dir', this.selectedLang === 'ar' ? 'rtl' : 'ltr');
+  }
+
+
+  ngAfterViewInit() {
+    const carouselElement = this.carousel.nativeElement;
+
+    carouselElement.addEventListener('slid.bs.carousel', (event: any) => {
+      const activeIndex = event.to;
+      console.log('Active slide index:', activeIndex);
+
+      const activeImg: HTMLImageElement | null = carouselElement.querySelector(
+        '.carousel-item.active img'
+      );
+      if (activeImg) {
+        console.log('Active image src:', activeImg.src);
+        this.navBackGround= activeImg.src
+        console.log('Active image alt:', activeImg.alt);
+      }
+    });
   }
 }
