87aec489ad44358a010182394f0300ced2b17d58
Ahmed Essam <40560225+iRashwan@users.noreply.github.com>
2025-06-13 21:48:08 +0300

    fix navbar for responsive

--- src/app/components/second-navbar/second-navbar.component.ts
+++ src/app/components/second-navbar/second-navbar.component.ts
@@ -1,6 +1,6 @@
 import { DOCUMENT, NgIf } from '@angular/common';
 import { Component, Inject, inject } from '@angular/core';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { LocalStorageServiceService } from '../../services/local-storage-service.service';
 import { TranslateServiceService } from '../../services/translate-service.service';
 import { Router } from '@angular/router';
@@ -18,21 +18,34 @@ export class SecondNavbarComponent {
   LocalStorageService = inject(LocalStorageServiceService);
   selectedLang: string = this.LocalStorageService.getItem('lang') ?? 'en';
   router = inject(Router);
+  translationService = inject(TranslateService);
+  isMenuOpen: boolean = false;
 
-  constructor(@Inject(DOCUMENT) private document: Document) {
+
+  constructor(@Inject(DOCUMENT) private document: Document,) {
     this.defaultLang();
   }
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
   toggleLanguage() {
     this.selectedLang = this.selectedLang === 'en' ? 'ar' : 'en';
     this.LocalStorageService.setItem('lang', this.selectedLang);
+    this.change(this.selectedLang);
+
     const html = document.getElementsByTagName('html')[0];
     const dir = this.selectedLang === 'ar' ? 'rtl' : 'ltr';
     html.setAttribute('dir', dir);
     html.setAttribute('lang', this.selectedLang);
 
     this.document.body.dir = dir;
+  }
+
+  change(language: string) {
+    this.translationService.use(language);
+    localStorage.setItem('lang', language);
   }
 
   defaultLang() {
