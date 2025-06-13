806113fb9ddb25ed2adf33e593a83385e1d43b7f
HebaNasser96 <heba.naser71996@gmail.com>
2025-06-13 20:33:45 +0300

    service details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MainServicesComponent } from './pages/main-services/main-services.component';
+import { ServiceDetailsComponent } from './pages/service-details/service-details.component';
 
 export const routes: Routes = [
     {
-        path:'',
-        pathMatch:'full',
-        redirectTo:'home'
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
     },
     {
-        path:'home',
-        component:HomeComponent
+        path: 'home',
+        component: HomeComponent
     },
-         {
-        path:'our-services',
-        component:MainServicesComponent
+    {
+        path: 'our-services',
+        component: MainServicesComponent,
     },
-   ];
+    {
+        path: 'our-services/details',
+        component: ServiceDetailsComponent
+    }
+];
