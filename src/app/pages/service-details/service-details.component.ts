c0b2a9d99561bc159588c7075339e04a74c255c2
HebaNasser96 <heba.naser71996@gmail.com>
2025-06-13 21:22:24 +0300

    working on service details

--- src/app/pages/service-details/service-details.component.ts
+++ src/app/pages/service-details/service-details.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { SecondNavbarComponent } from '../../components/second-navbar/second-navbar.component';
 
 @Component({
   selector: 'app-service-details',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe,SecondNavbarComponent],
   templateUrl: './service-details.component.html',
   styleUrl: './service-details.component.scss'
 })
